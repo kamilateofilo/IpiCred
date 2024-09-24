22526ba32a3a4200dfdfe9f737f8759674a844d5
Rayane Araújo <rayanearaujo28@hotmail.com>
2024-09-24 09:00:32 -0400

    style: responsivo politica de privacidade

--- src/Componetes/PrivacyPolicyText/styled.ts
+++ src/Componetes/PrivacyPolicyText/styled.ts
@@ -1,16 +1,25 @@
 import styled from "styled-components";
 
-
 export const PrivacyPolicyContainer = styled.div`
   background-color: #AC883F; 
   padding: 20px;
-  height: 324px;
+  height: auto;
+  min-height: 324px;
+
+  @media (max-width: 768px) {
+    padding: 15px;
+  }
 `;
 
 export const PrivacyPolicyTitle = styled.h1`
   color: #fff; 
   text-align: left;
-  margin-top: 10%;
+  margin-top: 12%;
+  
+  @media (max-width: 768px) {
+    font-size: 24px;
+    margin-top: 8em;
+  }
 `;
 
 export const PrivacyPolicyContent = styled.div`
@@ -18,8 +27,12 @@ export const PrivacyPolicyContent = styled.div`
    font-size: 16px;
    font-weight: 400;
    color: #000000;
-`;
 
+   @media (max-width: 768px) {
+     margin: 15px; 
+     font-size: 14px;
+   }
+`;
 
 export const ParagraphLGPD = styled.div`
     border: 1px solid #7F7667;
@@ -27,8 +40,12 @@ export const ParagraphLGPD = styled.div`
     font-weight: 500;
     margin-top: 3em;
 
-    >p{
+    > p {
         padding: 20px;
+
+        @media (max-width: 768px) {
+            padding: 15px;
+        }
     }
 `;
 
@@ -37,13 +54,25 @@ export const ContentTitle = styled.h3`
     color: #3F2409;
     font-size: 36px;
     font-weight: 500;
-`;
 
+    @media (max-width: 768px) {
+        font-size: 23px; 
+        margin-top: 20px;
+    }
+`;
 
 export const ContentParagraph = styled.p`
      letter-spacing: 0.5px;
+
+     @media (max-width: 768px) {
+         font-size: 14px;
+     }
 `;
 
 export const ContentList = styled.ul`
     letter-spacing: 0.5px;
+
+    @media (max-width: 768px) {
+        font-size: 14px;
+    }
 `;
